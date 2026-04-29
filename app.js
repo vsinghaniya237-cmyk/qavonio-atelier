d81b4dc739d3488b90cc16f1f4a1c1c18142477d
Vansh Singhaniya <vsinghaniya237@gmail.com>
2026-04-29 18:25:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -129,3 +129,12 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
+});
